ac1f6ef257bdc6171e8a65828ce4574a628d4286
Lukitas <lvd.follert@gmail.com>
2024-10-14 15:54:00 +0200

    last touches

--- psdcrossover/src/components/Navbar.jsx
+++ psdcrossover/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <li  className="pb-2 hover:border-b-4 border-primary"><a className="hover:font-bold" href="#">Speakers</a></li>
             <li  className="pb-2 hover:border-b-4 border-primary"><a className="hover:font-bold" href="#">Schedule</a></li>
         </ul>
-        <button className="bg-primary text-base-100 font-bold py-1.5 px-10 rounded-md">Contact Us</button>
+        <button className="bg-primary border-2 text-base-100 font-bold py-1.5 px-10 rounded-md hover:border-primary hover:border-2 hover:bg-base-100 hover:text-primary">Contact Us</button>
     </nav>
 </section>
     </>
